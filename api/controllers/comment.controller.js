ead89082762a3cd03e2026a1506230fb2730f309
Zachary Wagner <zwag14@yahoo.com>
2024-11-07 10:50:25 -0600

    show comments on posts

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -15,10 +15,21 @@ export const createComment = async (req, res, next) => {
             postId,
             userId,
         });
-        await newComment.dispatchEvent();
+        await newComment.save();
 
         res.status(200).json(newComment);
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const getPostComments = async (req, res, next) => {
+    try {
+      const comments = await Comment.find({ postId: req.params.postId }).sort({
+        createdAt: -1,
+      });
+      res.status(200).json(comments);
+    } catch (error) {
+      next(error);
+    }
+};
