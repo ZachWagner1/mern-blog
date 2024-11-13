539fc3fe97d9cb6509f9db11f1450ba2876f424c
Zachary Wagner <zwag14@yahoo.com>
2024-11-12 22:30:18 -0600

    About and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Zach's Blog
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo dolorem quas veritatis dolores? Ut eveniet tempora voluptate voluptatibus, harum autem ab. Earum aliquam quidem modi fugiat tempore? Omnis, fuga cum?
+            </p>
+
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto quas minus voluptas perferendis mollitia placeat libero vel, inventore necessitatibus doloremque, reprehenderit voluptatem vero adipisci veritatis quam, voluptatum molestias debitis aut.
+            </p>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum labore veniam ratione, esse rem veritatis ipsa animi corrupti nostrum debitis autem error enim inventore, similique deleniti, nobis deserunt earum eius.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
